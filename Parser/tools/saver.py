d76eed7921cfb74eeb4624521f842728f7ff7adc
MBkkt <32071355+MBkkt@users.noreply.github.com>
2018-12-09 14:38:46 +0200

    Add files via upload

--- Parser/tools/saver.py
+++ Parser/tools/saver.py
@@ -1,10 +1,34 @@
 import csv
+import sqlite3
+from itertools import chain
+import sys
+
+
+def save_sql(tabl):
+    """Сохраняем список в sql таблицу"""
+    ans = 1
+    with sqlite3.connect('./data/mydatabase.db') as conn:
+        cursor = conn.cursor()
+        try:
+            spec = 'pvp' if tabl[0] else 'pve'
+            tabl[1] = [tabl[1]]
+            tabl = tabl[1:]
+            tabl = list(chain.from_iterable(tabl))
+            if spec == 'pvp':
+                cursor.execute('INSERT INTO pvp VALUES (?,?,?,?,?,?,?,?,?,?,?)', tabl)
+            elif spec == 'pve':
+                cursor.execute('INSERT INTO pve VALUES (?,?,?,?,?,?,?,?,?,?,?)', tabl)
+            conn.commit()
+            ans = 0
+        except Exception as e:
+            print(e, file=sys.stderr)
+    return ans
 
 
 def save_csv(tabl):
     """Сохраняем список в csv файл"""
     FNAME = './data/' + ('table_pvp.csv' if tabl[0] else 'table_pve.csv')
-
+    ans = 1
     with open(FNAME, 'a', newline='', encoding='utf-8') as csvfile:
         f = csv.writer(csvfile, delimiter=';')
         try:
@@ -12,8 +36,9 @@ def save_csv(tabl):
                         tabl[2][0], tabl[2][1],
                         tabl[3][0], tabl[3][1],
                         tabl[4][0], tabl[4][1],
-                        tabl[5][0].replace(' ', '_'), tabl[5][1].replace(' ', '_'),
-                        tabl[6][0], tabl[6][1]), )
-            return 0
-        except Exception:
-            return 1
+                        tabl[5][0], tabl[5][1],
+                        tabl[6][0], tabl[6][1],))
+            ans = 0
+        except Exception as e:
+            print(e, file=sys.stderr)
+    return ans
